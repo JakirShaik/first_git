cf161b8fd4ea6239e0f2820e3f42ad50d6fb3a3c
Tekke <jtekke@deloitte.com>
2019-02-13 14:53:31 +0530

    added code

--- java-project/src/main/java/com/mkyong/hashing/App.java
+++ java-project/src/main/java/com/mkyong/hashing/App.java
@@ -9,9 +9,17 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        App a = new App();
+        a.method2();
     }
     public void method1() {
     	System.out.println("This method is modified by Developer 2");
     	System.out.println("This method added by Developer 1");
+    }
+    public void method2() {
+    	int i = 0;
+    	for(;i<=10; i++) {
+    		System.out.println(i);
+    	}
     }
 }
